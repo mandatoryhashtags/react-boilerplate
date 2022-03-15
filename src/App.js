c6136623003252f193ad868b664c213f515fd391
Curtis Schrum <curtis@trellico - cjschrum@gmail.com>
2022-03-15 10:12:34 -0500

    Initial push

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app__header">
+        <p>CJ React Template</p>
       </header>
+      <div className='main__content'>
+        <p className='main__content_title text-3xl font-bold underline'>This contains a few things already:</p>
+        <ul className='main__content__list'>
+          <li className='main__content__list__item'>React</li>
+          <li className='main__content__list__item'>TailwindCSS</li>
+          <li className='main__content__list__item'>Dockerfiles for a environment</li>
+          <li className='main__content__list__item'>.vscode Settings</li>
+          <li className='main__content__list__item'>ESLint Setup</li>
+          <li className='main__content__list__item'>.gitignore file</li>
+          <li className='main__content__list__item'>Folder structure for Gatsby if needed</li>
+          <li className='main__content__list__item'>We use Yarn</li>
+        </ul>
+      </div>
     </div>
   );
 }
